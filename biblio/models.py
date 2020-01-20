392ee8c2e1762b5ab7375f5590b9763ed401e622
helo <Jonathan.Hahn@t-online.de>
2020-01-20 23:04:08 +0100

    add validation for Copy model

--- biblio/models.py
+++ biblio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from .utils import CopyType
 
@@ -31,9 +32,24 @@ class BibEntry(models.Model):
 class Copy(models.Model):
     copyof = models.ForeignKey(BibEntry, on_delete=models.CASCADE)
     type = models.IntegerField(choices=CopyType.choices(), default=CopyType.REAL)
-    summary = models.TextField(default='', null=True, blank=True)
+    location = models.CharField(max_length=400, default='', null=True, blank=True)
     file = models.FileField(help_text="Use only if type is PDF!", 
                             blank=True, null=True, upload_to='pdfs')
 
     def get_copy_type_label(self):
         return CopyType(self.type).name.title()
+
+    def clean(self):
+        if self.type in (CopyType.REAL, CopyType.SOFTWARE):
+            if self.location=='' or self.location is None:
+                raise ValidationError('Location muss definiert sein für diesen Typ!')
+            if self.file is not None:
+                raise ValidationError('Eine Datei darf für diesen Medientyp nicht angegeben werden!')
+        # if type is file, file field is mandatory
+        if self.type==CopyType.PDF:
+            if self.file is None:
+                raise ValidationError('Für diesen Medientyp muss eine Datei angegeben werden!')
+            if self.location is not None and self.location!='':
+                raise ValidationError('Für diesen Medientyp darf keine Location angegeben werden! {self.location}')
+
+            
