4ef949234de9f4e488a946f7d32f371723e71b02
helo <Jonathan.Hahn@t-online.de>
2020-02-08 11:15:59 +0100

    gets sorting to work

---
+++ biblio/templatetags/stable_get.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def stable_get(context, **kwargs):
+    get = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        get[k] = v
+    return get.urlencode()
+
+
+@register.simple_tag(takes_context=True)
+def select_sort_class(context, id):
+    sort = context['request'].GET.get('sort')
+
+    if sort==id:
+        return 'sorted'
+    elif sort=='-'+id:
+        return 'sorted-desc'
+    
+    return ''
