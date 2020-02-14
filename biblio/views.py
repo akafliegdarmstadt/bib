140ca1817d12a193cc20102a0a465e2d79b0d5b7
helo <Jonathan.Hahn@t-online.de>
2020-02-14 15:54:58 +0100

    fix sorting

--- biblio/views.py
+++ biblio/views.py
@@ -26,7 +26,7 @@ class IndexView(LoginRequiredMixin, ListView):
 
         sort_val = self.request.GET.get('sort')
 
-        if sort_val and sort_val!='authors':
+        if sort_val and sort_val.replace('-','') in ('title', 'year'):
             return queryset.order_by(sort_val)
 
         return queryset
