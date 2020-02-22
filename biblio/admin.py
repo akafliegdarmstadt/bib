7273afdbc2b56755a81e4b0201abc3937544f3a6
helo <Jonathan.Hahn@t-online.de>
2020-02-22 13:53:42 +0100

    move copy to bibentry admin form

--- biblio/admin.py
+++ biblio/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import BibEntry, Author, Tag, Copy
 
-admin.site.register([BibEntry, Author, Tag, Copy])
+
+class CopyInline(admin.StackedInline):
+    model = Copy
+    extra = 0
+
+
+class BibEntryAdmin(admin.ModelAdmin):
+    inlines = [CopyInline]
+
+admin.site.register(BibEntry, BibEntryAdmin)
+admin.site.register([Author, Tag])
